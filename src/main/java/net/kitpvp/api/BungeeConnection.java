5dd1a57d7fd03ba1d91d30db266f4c982d23c0fa
f <moritz.hennen@gmail.com>
2020-06-04 18:25:42 +0200

    Add BungeeConnection#getRanks

--- src/main/java/net/kitpvp/api/BungeeConnection.java
+++ src/main/java/net/kitpvp/api/BungeeConnection.java
@@ -1,5 +1,8 @@
 package net.kitpvp.api;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
 
@@ -7,4 +10,5 @@ public interface BungeeConnection {
 
     void getRank(UUID uuid, Consumer<Group> callback);
 
+    void getRanks(List<UUID> uuids, Consumer<Map<UUID, Group>> callback);
 }
