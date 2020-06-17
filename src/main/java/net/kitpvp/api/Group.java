8e915b6c860d1ffb57a7e6971a125a60da41c645
f <moritz.hennen@gmail.com>
2020-06-17 21:02:29 +0200

    Revert #1562fff3 "Add PluginResolver api to remove spigot as a dependency"

--- src/main/java/net/kitpvp/api/Group.java
+++ src/main/java/net/kitpvp/api/Group.java
@@ -2,8 +2,10 @@ package net.kitpvp.api;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import net.kitpvp.api.impl.PluginResolver;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
@@ -159,11 +161,24 @@ public enum Group {
         return api.getGroupManager().getPlayerGroup(playerId);
     }
 
+    public static Group getGroup(Player player) {
+        return getGroup(player.getUniqueId());
+    }
+
+    public static Group getGroup(CommandSender sender) {
+        init();
+
+        if(api == null)
+            return DEFAULT;
+
+        return api.getGroupManager().getPlayerGroup(sender);
+    }
+
     private static void init() {
         if(init)
             return;
 
         init = true;
-        api = PluginResolver.getResolver().getAPI();
+        api = (NetworkAPI) Bukkit.getPluginManager().getPlugin("PluginAPI");
     }
 }
--- src/main/java/net/kitpvp/api/group/IGroupManager.java
+++ src/main/java/net/kitpvp/api/group/IGroupManager.java
@@ -1,6 +1,8 @@
 package net.kitpvp.api.group;
 
 import net.kitpvp.api.Group;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
@@ -8,7 +10,7 @@ public interface IGroupManager {
 
     Group getPlayerGroup(UUID playerId);
 
-    /*Group getPlayerGroup(Player player);
+    Group getPlayerGroup(Player player);
 
-    Group getPlayerGroup(CommandSender sender);*/
+    Group getPlayerGroup(CommandSender sender);
 }
