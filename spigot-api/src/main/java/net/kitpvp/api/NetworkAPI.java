d40425a6effb5229106fe56dc38f35a3f71a64f6
f <moritz.hennen@gmail.com>
2020-06-18 14:14:30 +0200

    Remove MongoDB library

--- spigot-api/src/main/java/net/kitpvp/api/NetworkAPI.java
+++ spigot-api/src/main/java/net/kitpvp/api/NetworkAPI.java
@@ -5,6 +5,7 @@ import net.kitpvp.api.group.IGroupManager;
 import net.kitpvp.api.user.User;
 import net.kitpvp.api.user.UserFactory;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
@@ -12,16 +13,24 @@ public interface NetworkAPI {
 
     UserFactory<? extends User> getUserFactory();
 
-    default User getUser(UUID playerId) {
-        return getUserFactory().getUser(playerId);
-    }
-
     IBungeeConnection getBungeeConnection();
 
     IGroupManager getGroupManager();
 
+    default User getUser(UUID playerId) {
+        return getUserFactory().getUser(playerId);
+    }
+
+    default Group getGroup(Player player) {
+        return getGroupManager().getPlayerGroup(player);
+    }
+
     @SuppressWarnings("unchecked")
     static <T extends NetworkAPI> T getAPI() {
         return (T) Bukkit.getPluginManager().getPlugin("PluginAPI");
+    }
+
+    static Group getPlayerGroup(Player player) {
+        return getAPI().getGroup(player);
     }
 }
