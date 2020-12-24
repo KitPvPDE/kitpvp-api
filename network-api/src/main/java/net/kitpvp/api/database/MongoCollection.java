2e7cd8cf7ea1156260d301cccf5d4cea3202a575
f <moritz.hennen@gmail.com>
2020-12-24 16:15:35 +0100

    added MongoCollection enum to api

---
+++ network-api/src/main/java/net/kitpvp/api/database/MongoCollection.java
@@ -0,0 +1,35 @@
+package net.kitpvp.api.database;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import net.kitpvp.mongodbapi.database.Collection;
+
+@RequiredArgsConstructor
+public enum MongoCollection implements Collection {
+
+    // network
+    HEADS("network_heads", "public_db"),
+
+    // kitpvp
+    KITPVP_STATS("stats", "kitpvp_db"),
+
+    // norisk
+    NORISK_USERS("users", "norisk_db"),
+
+    // web
+    WEB_USERS("users", "website_db"),
+
+    // discord
+    DC_USERS("users", "discord_db"),
+    ;
+
+    @Getter
+    private final String collectionName;
+    @Getter
+    private final String databaseName;
+
+    MongoCollection(String collectionName) {
+        this(collectionName, "kitpvp_db");
+    }
+
+}
